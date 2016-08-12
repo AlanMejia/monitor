6d5fd01ba88e0f2a445d48834f8e6dd517a9fb57
alandaet <dispelcaster.admv@gmail.com>
2016-08-12 21:41:57 +0000

    security

--- src/main/java/dummy/HelloController.java
+++ src/main/java/dummy/HelloController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.security.access.annotation.Secured;
 
 @Controller
 public class HelloController {
