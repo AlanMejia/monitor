f7b97aff3be5d7a44095eb47c821cb8c0fd5f4fe
alandaet <dispelcaster.admv@gmail.com>
2016-08-29 14:14:19 +0000

    fixing bugs

--- src/main/java/dummy/FlagObject.java
+++ src/main/java/dummy/FlagObject.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class FlagObject {
 
-    private String flag;
+    protected String flag;
 
     public String getFlag() {
         return flag;
